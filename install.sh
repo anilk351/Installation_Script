023bc3fb2d1ec055a61bb288474cd65b1f6c0d78
Anil shinde <shindeanil351@gmail.com>
2024-01-24 17:48:37 +0530

    added dolphin explorer and naps scaner

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 echo please wait while installtion is complete
 
 #install software that required for epson printer
-sudo apt -y install lsb lsb-core diodon goldendict goldendict-wordnet openssh-server net-tools wdtokentool-proxkey
+sudo apt -y install lsb lsb-core diodon goldendict goldendict-wordnet openssh-server net-tools wdtokentool-proxkey dolphin
+
+#download naps2 software
+wget https://github.com/cyanfish/naps2/releases/download/v7.3.0/naps2-7.3.0-linux-x64.deb
 
 #install software
-sudo dpkg -i epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb 
+sudo dpkg -i epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb naps2-7.3.0-linux-x64.deb
 
 #install epson script
 sudo ./epsonscan2-bundle-6.7.61.0.x86_64.deb/install.sh
