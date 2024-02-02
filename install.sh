91bc9d429d6063b898f5ff1e08854013c923aa3a
Anilk <shindeanil351@gmail.com>
2024-02-02 14:49:07 +0530

    lot of changes were made

--- install.sh
+++ install.sh
@@ -9,16 +9,13 @@ sudo apt -y install lsb lsb-core diodon goldendict goldendict-wordnet openssh-se
 wget https://github.com/anilk351/installation_Script/raw/main/files.zip
 
 #files unzip 
-unzip files
-
-#now go to files folder
-cd files
+sudo unzip files
 
 #install software
-sudo dpkg -i epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb naps2-7.3.0-linux-x64.deb proxkey_ubantu.deb
+sudo dpkg -i ./files/epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb ./files/proxkey_ubantu.deb
 
-#install epson script added 
-sudo ./epsonscan2-bundle-6.7.61.0.x86_64.deb/install.sh
+# Corrected path to the install script
+sudo ./files/epsonscan2-bundle-6.7.61.0.x86_64.deb/install.sh
 
 sudo apt purge ipp-usb -y
 
