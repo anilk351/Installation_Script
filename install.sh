15f1e70657b165b5e513695401107a9759de5dae
Anilk <shindeanil351@gmail.com>
2024-02-02 14:55:47 +0530

    added naps scaner and did not know when i deleted it

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ echo please wait while installtion is complete
 #install software that required for epson printer
 sudo apt -y install lsb lsb-core diodon goldendict goldendict-wordnet openssh-server net-tools dolphin
 
+#naps2 download
+wget https://github.com/cyanfish/naps2/releases/download/v7.3.0/naps2-7.3.0-linux-x64.deb
+
 #download all the files in zip mode to install
 wget https://github.com/anilk351/installation_Script/raw/main/files.zip
 
 #files unzip 
-sudo unzip files
+unzip files
 
 #install software
-sudo dpkg -i ./files/epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb ./files/proxkey_ubantu.deb
+sudo dpkg -i ./files/epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb ./files/proxkey_ubantu.deb naps2-7.3.0-linux-x64.deb
 
 # Corrected path to the install script
 sudo ./files/epsonscan2-bundle-6.7.61.0.x86_64.deb/install.sh
@@ -27,7 +30,7 @@ echo "This script will downgrade wpa_supplicant on your system. Do you want to c
 read answer
 
 if [ "$answer" != "y" ]; then
-    echo "Aborted. No changes were made."
+    echo "Aborted. No changes were made in wifi client."
     exit 0
 fi
 
