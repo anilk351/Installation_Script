c2f17bd5d64c6f8a2a1711199d3e3978c4ee24ca
anilk <shindeanil351@gmail.com>
2024-02-28 13:10:57 +0530

    added choose option for install app and wifi client

--- install.sh
+++ install.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
-echo please wait while installtion is complete
 
-#install software that required for epson printer
-sudo apt -y install lsb lsb-core diodon goldendict goldendict-wordnet openssh-server net-tools dolphin
 
-#naps2 download
-wget https://github.com/cyanfish/naps2/releases/download/v7.3.0/naps2-7.3.0-linux-x64.deb
+install_apps() 
+{
+	echo please wait while installtion is complete
 
-#download all the files in zip mode to install
-wget https://github.com/anilk351/Storage_Files/raw/main/files.zip
+	#install software that required for epson printer
+	sudo apt -y install lsb lsb-core diodon goldendict goldendict-wordnet openssh-server net-tools dolphin
 
-#files unzip 
-unzip files
+	#naps2 download
+	wget https://github.com/cyanfish/naps2/releases/download/v7.3.0/naps2-7.3.0-linux-x64.deb
 
-#install software
-sudo dpkg -i ./files/epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb ./files/proxkey_ubantu.deb naps2-7.3.0-linux-x64.deb
+	#download all the files in zip mode to install
+	wget https://github.com/anilk351/Storage_Files/raw/main/files.zip
 
-# Corrected path to the install script
-sudo sh ./files/epsonscan2-bundle-6.7.61.0.x86_64.deb/install.sh
+	#files unzip 
+	unzip files
 
-sudo apt purge ipp-usb -y
+	#install software
+	sudo dpkg -i ./files/epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb ./files/proxkey_ubantu.deb naps2-7.3.0-linux-x64.deb
 
-echo The Installation of scaner driver, proxykey and naps2 is done
+	# Corrected path to the install script
+	sudo sh ./files/epsonscan2-bundle-6.7.61.0.x86_64.deb/install.sh
 
-sleep 5
+	sudo apt purge ipp-usb -y
+
+	echo The Installation of scaner driver, proxykey and naps2 is done
+
+	sleep 5
+
+}
+
+
+echo "Do you want to install required apps and NAPS2? (yes/no)"
+read install_choice
+
+if [ "$install_choice" = "y" ]; then
+    install_apps
+fi
 
 echo "This script will downgrade wpa_supplicant on your system. Do you want to continue? (yes/no)"
 read answer
