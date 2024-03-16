be1390e909571fd521301f56651202f8e923d1f4
anilk <shindeanil351@gmail.com>
2024-03-16 11:10:15 +0530

    removed wifi completely

--- install.sh
+++ install.sh
@@ -32,43 +32,14 @@ install_apps() {
     
 }
 
-install_wpa() {
-    echo "This will downgrade wpa_supplicant to version 2.9.0-21build1."
-
-    # Add old-releases repository to sources.list
-    echo "Adding old-releases repository to sources.list..."
-    sudo tee -a /etc/apt/sources.list >/dev/null <<EOF
-deb http://old-releases.ubuntu.com/ubuntu/ impish main restricted universe multiverse
-deb http://old-releases.ubuntu.com/ubuntu/ impish-updates main restricted universe multiverse
-deb http://old-releases.ubuntu.com/ubuntu/ impish-security main restricted universe multiverse
-EOF
-
-    # Update package list and downgrade wpa_supplicant
-    echo "Updating package list and downgrading wpa_supplicant..."
-    sudo apt update
-    sudo apt --allow-downgrades install wpasupplicant=2:2.9.0-21build1 -y
-
-    # Mark the package to prevent updates
-    echo "Marking wpasupplicant to prevent updates..."
-    sudo apt-mark hold wpasupplicant
-
-    echo "Downgrade complete. wpasupplicant is now held at version 2.9.0-21build1."
-}
-
 # Main script starts here
 
-echo "Do you want to install required apps and NAPS2? (yes/no)"
+echo "Do you want to install required Epson printer driver and apps and NAPS2? (yes/no)"
 read install_choice
 
 if [ "$install_choice" = "yes" ]; then
     install_apps
 fi
 
-echo "Do you want to downgrade wpa client Wifi? (yes/no)"
-read choice
-
-if [ "$choice" = "yes" ]; then
-    install_wpa
-fi
 
 
