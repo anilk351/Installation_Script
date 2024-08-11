64eaed12d06b8e1829acf845d5c92af2b6e6deff
bashking <shindeanil351@gmail.com>
2024-08-11 23:30:44 +0530

    changed every logic

--- install.sh
+++ install.sh
@@ -1,69 +1,236 @@
 #!/bin/bash
 
-downgrade_Wifi()
+curdt=$(date +%d_%m_%Y)
 
+log_file="install_script_"${curdt}".log"
+
+log_file="/var/log/my_script.log"
+
+log() 
 {
-    #downgrade wifi for dell system DELL 7010
+    local message="$1"
+    local print_to_shell="$2"
+
+    # Format the log message
+    local log_entry="$(date '+%Y-%m-%d %H:%M:%S') - $message"
+
+    # Write the log entry to the file
+    echo "$log_entry" >> "$log_file"
+
+    # Optionally print the log entry to the shell
+    if [ "$print_to_shell" == "true" ]; then
+        echo "$log_entry"
+    fi
+}
+
+
+tasks=(
+    "Install Naps Scanner"
+    "Install Only Epson Driver and Epson Scanner"
+    "Install Fijustu Scanner Driver"
+    "Install other Ubuntu Apps (e.g.-Dictionary)"
+    "Downgrade wifi only for Dell 7010(model)"
+)
+
+check_dependency() {
+    for cmd in "$@"; do
+        if ! command -v "$cmd" &> /dev/null; then
+            echo "$cmd could not be found, please install it."
+            exit 1
+        fi
+    done
+}
+
+confirm() {
+    read -p "Are you sure you want to proceed? [y/N]: " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+zip_files()
+{
+
+        # Check if the extracted directory exists
+    if [ -d "./files" ]; then
+        echo "Files already extracted. Using existing files."
+    elif [ -f "files.zip" ]; then
+        echo "files.zip found but not extracted. Extracting files..."
+        unzip -q files.zip
+    else
+        echo "Downloading new files.zip..."
+        wget -q https://github.com/anilk351/Storage_Files/raw/main/files.zip
+
+        echo "Unzipping files.zip..."
+        unzip -q files.zip
+    fi
+}
+
+install_naps() 
+{
+    echo "This will Install Naps Scanner"
+    if confirm; 
+    then
+        keyring_path="/etc/apt/keyrings/naps2.gpg"
+
+        # Check if the keyring file already exists
+        if [ -f "$keyring_path" ]; then
+            log "NAPS2 public key already exists. Skipping download."
+        else
+            log "Downloading and adding NAPS2 public key..."
+            curl -fsSL https://www.naps2.com/naps2-public.pgp | sudo gpg --dearmor -o "$keyring_path"
+        fi
+
+        # Check if the NAPS2 source is already in the sources list
+        if ! grep -q "^deb .*$keyring_path" /etc/apt/sources.list.d/naps2.list 2>/dev/null; then
+            log "Adding NAPS2 as an Apt source..."
+            echo "deb [signed-by=$keyring_path] https://downloads.naps2.com ./" | sudo tee /etc/apt/sources.list.d/naps2.list >/dev/null
+        else
+            log "NAPS2 source already exists in sources.list."
+        fi
+        sudo apt update && sudo apt install naps2 || 
+        {
+        log "Error installing NAPS2"
+        exit 1
+        }
+        log "NAPS2 installation completed" true
+    else
+        echo "Installation is Cancelled by user"
+    fi        
+}
+
+install_epson() 
+{
+    echo "This will Install Epson Driver "
+    if confirm; 
+    then
+         # Install required packages
+        sudo apt update && sudo apt -y lsb lsb-core || 
+        {
+        log "Error Installing lsb core"
+        exit 1
+        }
+        log  "lsb core for epson installed sucessfully"
+
+        zip_files
+
+        # Install necessary software
+        sudo dpkg -i ./files/epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb ||
+        {
+            log "Error installing epson printer Driver file"
+        }
+            log "sucessfully installed Epson Driver file"
+
+        # Run installation script for Epson scanner
+        sudo sh ./files/epsonscan2-bundle-6.7.61.0.x86_64.deb/install.sh ||
+         {
+            log "Error installing epson printer scanner file"
+        }
+            log "sucessfully installed Epson scanner file"
+
+        # Remove unnecessary package
+        sudo apt purge ipp-usb -y || 
+        {
+            log "Error removing ipp-usb pakage"
+        }
+            log "sucessfully removing ipp-usb pakage"
+
+        log "The Driver installation Epson is complete." true
+            
+         # proceed with installation
+    else
+        log "Installation canceled by user"
+    fi
     
-
-
 }
 
-install_naps()
+install_fijustu()
 {
-    # Download the NAPS2 public key
-    curl -fsSL https://www.naps2.com/naps2-public.pgp | sudo gpg --dearmor -o /etc/apt/keyrings/naps2.gpg
 
-    # Add NAPS2 as an Apt source
-    echo "deb [signed-by=/etc/apt/keyrings/naps2.gpg] https://downloads.naps2.com ./" | sudo tee /etc/apt/sources.list.d/naps2.list >/dev/null
+    if confirm; 
+    then
+    zip_files
 
-    # Install NAPS2
-    sudo apt update
-    sudo apt install naps2
+        #Install fijustu driver
+        echo "The development is on the way"
+    else
+        echo "Installation canceled by user"
+    fi
 
 }
 
-install_apps() {
-    echo "Please wait while installation is in progress..."
+
+downgrade_Wifi()
+{    
+    
+    if confirm; 
+    then
+        #downgrade wifi for dell system DELL 7010
+        echo "The development is on the way"
+    else
+        echo "Installation canceled by user"
+    fi
+
+}
+
+
+install_apps() 
+{
+    echo "In this Installaion following apps are going to install"
+    echo -e "Golden_Dictionary\nProxykey\nDolphine_File_manager\nClipboard\nNet-tools\nOpenSSh-server"
 
     # Install required packages
-    sudo apt update
-    sudo apt -y install lsb lsb-core diodon goldendict goldendict-wordnet openssh-server net-tools dolphin
+    sudo apt update && sudo apt -y install diodon goldendict goldendict-wordnet openssh-server net-tools dolphin || 
+     {
+            log "Error installing apps please check indivisually"
+        }
+            log "sucessfully installed apps"
 
-    # Download additional files
-    wget -q https://github.com/anilk351/Storage_Files/raw/main/files.zip
-
-    # Unzip files
-    unzip -q files.zip
-
-    # Install necessary software
-    sudo dpkg -i ./files/epson-inkjet-printer-escpr2_1.2.3-1_amd64.deb ./files/proxkey_ubantu.deb naps2-7.3.1-linux-x64.deb
-
-    # Run installation script for Epson scanner
-    sudo sh ./files/epsonscan2-bundle-6.7.61.0.x86_64.deb/install.sh
-
-    # Remove unnecessary package
-    sudo apt purge ipp-usb -y
     
-    sudo rm -r ~/.anydesk/
+    # Install Proxykey ubuntu software
+    zip_files
 
-    echo "The installation of scanner driver, proxykey, and NAPS2 is complete."
-    
-    
-    sleep 3
+    sudo dpkg -i ./files/proxkey_ubantu.deb ||
+     {
+            log "Error installing proxykey for ubuntu"
+        }
+            log "sucessfully installed proxykey for ubuntu"
+
+    echo -e "The installation , proxykey complete.\nGolden_Dictionary\nProxykey\nDolphine_File_manager\nClipboard\nNet-tools\nOpenSSh-server"
     
 }
 
-# Main script starts here
+execute_task() {
+    case $1 in
+        1) install_naps ;;
+        2) install_epson ;;
+        3) install_fijustu ;;
+        4) install_apps ;;
+        5) downgrade_Wifi ;;
+        *) echo "Invalid entry. Please try again." ;;
+    esac
+}
 
-echo "Do you want to install required Epson printer driver and apps and NAPS2? (yes/no)"
-read install_choice
+# Main script
 
+check_dependency "curl" "wget" "unzip"
 
+PS3="Select option by number: "
 
-if [ "$install_choice" = "yes" ]; then
-    install_apps
-fi
-
-
-
+select option in "${tasks[@]}" "exit"
+do
+    if [[ $REPLY -le ${#tasks[@]} ]]; then
+        execute_task $REPLY
+    elif [[ $REPLY == $(( ${#tasks[@]} + 1 )) ]]; 
+    then
+        echo "Exiting..."
+        break
+    else
+        echo "Invalid entry. Please try again."
+    fi
+done
